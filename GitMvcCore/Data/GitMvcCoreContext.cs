7d65cb197f2a0ea4d642fd4eaf2b62f51756ffc7
Jon-blk <jon.svenonius@gmail.com>
2019-01-22 16:45:22 +0100

    Data model added

---
+++ GitMvcCore/Data/GitMvcCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GitMvcCore.Models
+{
+    public class GitMvcCoreContext : DbContext
+    {
+        public GitMvcCoreContext (DbContextOptions<GitMvcCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GitMvcCore.Models.Car> Car { get; set; }
+    }
+}
---
+++ GitMvcCore/Migrations/20190122154209_initial.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GitMvcCore.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    CarId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    DriverId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Car_Person_DriverId",
+                        column: x => x.DriverId,
+                        principalTable: "Person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Car_DriverId",
+                table: "Car",
+                column: "DriverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_CarId",
+                table: "Person",
+                column: "CarId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Person_Car_CarId",
+                table: "Person",
+                column: "CarId",
+                principalTable: "Car",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Car_Person_DriverId",
+                table: "Car");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
