38fda5efa5ac8dd4d5352dff8795fc80f692bff9
Jon-blk <jon.svenonius@gmail.com>
2019-01-23 09:18:33 +0100

    routing changed

--- GitMvcCore/Startup.cs
+++ GitMvcCore/Startup.cs
@@ -60,9 +60,7 @@ namespace GitMvcCore
 
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute();
             });
         }
     }
